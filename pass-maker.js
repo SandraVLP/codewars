46586e17d5057d49b9cd3459d1ac460c315d4cb8
SandraVLP <aleksanvp@yandex.ru>
2024-01-21 19:44:18 +0300

    редактирование

--- pass-maker.js
+++ pass-maker.js
@@ -4,3 +4,7 @@
 function makePassword(phrase) {
 return phrase.split(' ').map(v=>v.slice(0,1)).join('').replace(/(o)/gi,0).replace(/(s)/gi,5).replace(/(i)/gi,1)
 }
+
+function makePassword(phrase) {
+    return phrase.split(' ').map(v=>v.slice(0,1)).join('').replace(/(o)/gi,0).replace(/(s)/gi,5).replace(/(i)/gi,1)
+    }
